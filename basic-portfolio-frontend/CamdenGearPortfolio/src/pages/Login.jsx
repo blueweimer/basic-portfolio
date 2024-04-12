2f7e3b1d467a70bdd8a89849cdde8d60e0cac3ee
Camden Gear <cbg47800@ucmo.edu>
2024-04-12 17:24:27 -0500

    Commit Message

--- basic-portfolio-frontend/CamdenGearPortfolio/src/pages/Login.jsx
+++ basic-portfolio-frontend/CamdenGearPortfolio/src/pages/Login.jsx
@@ -36,7 +36,32 @@ const Login = () => {
 
 
 
-  return <div>Login</div>;
+  return(
+            <div>
+            <div className="auth">
+                <h1>Login</h1>
+                <form>
+                  <input
+                    type="text"
+                    placeholder="username"
+                    name="username"
+                    onChange={handleChange}
+                  />
+                  <input
+                    type="password"
+                    placeholder="password"
+                    name="password"
+                    onChange={handleChange}
+                  />
+                  <button onClick={handleSubmit}>Login</button>
+                  {err && <p>{err}</p>}
+                  <span>
+                    Don't you have an account? <Link to="/register">Register</Link>
+                  </span>
+                </form>
+              </div>
+            </div>
+  );
 };
 
 export default Login;
